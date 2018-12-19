fa335eb3dbca5698c439055943a197121662d20a
17JavieraDiaz <43445003+17JavieraDiaz@users.noreply.github.com>
2018-12-19 14:58:43 -0500

    khb

--- DynamicPicBoxJav/DynamicPicBoxJav/DynamicPicBoxJavForm.Designer.cs
+++ DynamicPicBoxJav/DynamicPicBoxJav/DynamicPicBoxJavForm.Designer.cs
@@ -28,20 +28,100 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lblInstructons = new System.Windows.Forms.Label();
+            this.picWalkMan = new System.Windows.Forms.PictureBox();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picWalkMan)).BeginInit();
             this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("OCR A Std", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(346, 264);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(83, 41);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(812, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Font = new System.Drawing.Font("OCR A Std", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(55, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // lblInstructons
+            // 
+            this.lblInstructons.AutoSize = true;
+            this.lblInstructons.Font = new System.Drawing.Font("OCR A Std", 12F);
+            this.lblInstructons.Location = new System.Drawing.Point(149, 24);
+            this.lblInstructons.Name = "lblInstructons";
+            this.lblInstructons.Size = new System.Drawing.Size(488, 17);
+            this.lblInstructons.TabIndex = 2;
+            this.lblInstructons.Text = "Click on a  picture to make the man walk";
+            // 
+            // picWalkMan
+            // 
+            this.picWalkMan.Location = new System.Drawing.Point(329, 545);
+            this.picWalkMan.Name = "picWalkMan";
+            this.picWalkMan.Size = new System.Drawing.Size(100, 50);
+            this.picWalkMan.TabIndex = 3;
+            this.picWalkMan.TabStop = false;
             // 
             // frmDynamicPicBoxJav
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(495, 433);
+            this.ClientSize = new System.Drawing.Size(812, 607);
+            this.Controls.Add(this.picWalkMan);
+            this.Controls.Add(this.lblInstructons);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "frmDynamicPicBoxJav";
             this.Text = "Dynamic PictureBox by Javiera Diaz";
+            this.Load += new System.EventHandler(this.frmDynamicPicBoxJav_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picWalkMan)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Label lblInstructons;
+        private System.Windows.Forms.PictureBox picWalkMan;
     }
 }
 
--- DynamicPicBoxJav/DynamicPicBoxJav/DynamicPicBoxJavForm.cs
+++ DynamicPicBoxJav/DynamicPicBoxJav/DynamicPicBoxJavForm.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
 
 namespace DynamicPicBoxJav
 {
@@ -15,6 +16,123 @@ namespace DynamicPicBoxJav
         public frmDynamicPicBoxJav()
         {
             InitializeComponent();
+        }
+        //Procedure:
+        //Input:
+        //Output:
+        //Desc:
+        private void GeneratePictureBoxes()
+        {
+            //generate these puicture boxes on these coordinates on the form
+            GeneratePictureBox(61, 78);
+            GeneratePictureBox(464, 78);
+            GeneratePictureBox(61, 358);
+            GeneratePictureBox(464, 358);
+        }
+        //Procedure:
+        //Input:
+        //Output:
+        //Desc:
+        private void PictureBox_Click(object sender, EventArgs e)
+        {
+            //cast sender object in picturebox
+            PictureBox tmpPicMan = (PictureBox)sender;
+            //write message to ocnsole to check when picture box was clicked
+            Console.WriteLine("Picture box (" + tmpPicMan.Location.X + "," + tmpPicMan.Location.Y + "( was clicked");
+
+            //declare contants and variables
+            const byte MAX_FRAMES = 10;
+            int pictureFrameCounter = 1;
+
+            //continue loop while frame counter is less than the max that there is to display
+            while (pictureFrameCounter < MAX_FRAMES + 1)
+            {
+                if (pictureFrameCounter == 1)
+                {
+                    tmpPicMan.Image = Properties.Resources.walk1;
+                }
+                else if (pictureFrameCounter == 2)
+                {
+                    tmpPicMan.Image = Properties.Resources.walk2;
+                }
+                else if (pictureFrameCounter == 3)
+                {
+                    tmpPicMan.Image = Properties.Resources.walk3;
+                }
+                else if (pictureFrameCounter == 4)
+                {
+                    tmpPicMan.Image = Properties.Resources.walk4;
+                }
+                else if (pictureFrameCounter == 5)
+                {
+                    tmpPicMan.Image = Properties.Resources.walk5;
+                }
+                else if (pictureFrameCounter == 6)
+                {
+                    tmpPicMan.Image = Properties.Resources.walk6;
+                }
+                else if (pictureFrameCounter == 7)
+                {
+                    tmpPicMan.Image = Properties.Resources.walk7;
+                }
+                else if (pictureFrameCounter == 8)
+                {
+                    tmpPicMan.Image = Properties.Resources.walk8;
+                }
+                else if (pictureFrameCounter == 9)
+                {
+                    tmpPicMan.Image = Properties.Resources.walk9;
+                }
+                else if (pictureFrameCounter == 10)
+                {
+                    tmpPicMan.Image = Properties.Resources.walk10;
+                }
+            }
+        }
+        //Procedure:
+        //Input:
+        //Output:
+        //Desc:
+        private void GeneratePictureBox(int x, int y)
+        {
+            //dymically generate a new picture box and a new point at the given location (x, y)
+            PictureBox tmpPicMan = new PictureBox();
+            Point pntPic = new System.Drawing.Point(x, y);
+
+            //set locaton of picturebox
+            tmpPicMan.Location = pntPic;
+
+            //assign omage to picture box
+            tmpPicMan.Image = Properties.Resources.walk1;
+
+            //set picture box mode to stretch size
+            tmpPicMan.SizeMode = PictureBoxSizeMode.StretchImage;
+
+            //make picturebox width and height to be same as the image
+            tmpPicMan.ClientSize = new Size(Properties.Resources.walk1.Width, Properties.Resources.walk1.Height);
+
+            tmpPicMan.Click += new System.EventHandler(PictureBox_Click);
+;
+            //add picturebox to form
+            this.Controls.Add(tmpPicMan);
+        }
+        private void frmDynamicPicBoxJav_Load(object sender, EventArgs e)
+        {
+            //hide this label
+            this.lblInstructons.Hide();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            //show this labe;
+            this.lblInstructons.Show();
+            //call this function to generate the 4 picture boxes
+            GeneratePictureBoxes();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
